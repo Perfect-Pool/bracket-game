91cafbf06d4501a73f2440fbcc547ed39d6b5feb
Gabriel Nunes <gabesan@me.com>
2024-06-27 15:07:06 -0300

    New contract for olympics testnet data

--- verificar.sh
+++ verificar.sh
@@ -3,7 +3,7 @@
 echo "Iniciando a verificação dos scripts..."
 
 # yarn verify-brackets base-testnet
-# yarn verify-olympics base-testnet
+yarn verify-olympics base-testnet
 # yarn verify-nft base-testnet
 # yarn verify-nft-olympics base-testnet
 
@@ -13,6 +13,6 @@ echo "Iniciando a verificação dos scripts..."
 # yarn verify-library-1 base-testnet
 # yarn verify-library-2 base-testnet
 # yarn verify-library-3 base-testnet
-yarn verify-library-flags base-testnet
+# yarn verify-library-flags base-testnet
 
 echo "Verificação concluída."
