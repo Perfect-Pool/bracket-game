69454bad63bb5c8abf11968c0a7e1de22575b401
Gabriel Nunes <gabesan@me.com>
2024-07-04 19:41:24 -0300

    chore: Update contract addresses in tenderly.yaml and enable verify-nft-olympics in verificar.sh

--- verificar.sh
+++ verificar.sh
@@ -5,9 +5,9 @@ echo "Iniciando a verificação dos scripts..."
 # yarn verify-brackets base-testnet
 yarn verify-olympics base-testnet
 # yarn verify-nft base-testnet
-# yarn verify-nft-olympics base-testnet
+yarn verify-nft-olympics base-testnet
 
-yarn verify-nft-metadata base-testnet
+# yarn verify-nft-metadata base-testnet
 # yarn verify-nft-image base-testnet
 
 # yarn verify-library-1 base-testnet
